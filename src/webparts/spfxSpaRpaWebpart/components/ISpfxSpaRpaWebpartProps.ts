e8872a674d2d359d72667545c9e9bcda90144789
Juan Alberto Nuñez Rodriguez <juanalberto.nunez@hotmail.com>
2024-11-01 15:27:34 -0600

    initial spfx

---
+++ src/webparts/spfxSpaRpaWebpart/components/ISpfxSpaRpaWebpartProps.ts
@@ -0,0 +1,7 @@
+export interface ISpfxSpaRpaWebpartProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/spfxSpaRpaWebpart/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ISpfxSpaRpaWebpartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'SpfxSpaRpaWebpartWebPartStrings' {
+  const strings: ISpfxSpaRpaWebpartWebPartStrings;
+  export = strings;
+}
